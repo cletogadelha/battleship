11ee56be966859bb6510f7532a6431d88f6b8fb6
Cleto Gadelha <cletogadelha@gmail.com>
2017-05-05 11:42:00 -0300

    adding test configs

--- src/test/java/com/cletogadelha/GameControllerTest.java
+++ src/test/java/com/cletogadelha/GameControllerTest.java
@@ -2,27 +2,18 @@ package com.cletogadelha;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.cletogadelha.controller.GameController;
-import com.cletogadelha.service.GameService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(GameController.class)
+@SpringBootTest
+@AutoConfigureTestDatabase
 public class GameControllerTest {
 	
-	 @Autowired
-	 MockMvc mockMvc;
-	 
-	 @MockBean
-	 GameService gameServiceMock;
-	 
 	 @Test
 	 public void getGameTest() throws Exception {
+		 
 	 }
 	 
 
