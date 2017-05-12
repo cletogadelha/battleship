17fa77861ede6fcf799ce7a52fe9e7d21aca9fd3
Cleto Gadêlha <cletogadelha@gmail.com>
2017-05-12 11:33:15 -0300

    mrge

--- src/test/resources/data.sql
+++
@@ -1,7 +0,0 @@
---INSERT INTO player(player_id, created, updated, version, losses, name, score, wins)
---	VALUES ('1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 'Test Player', 0, 0);
-	
-INSERT INTO player(player_id, created, updated, version, losses, name, score, wins)
-	VALUES (3, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 'Test Player4', 0, 0);
-
-	
